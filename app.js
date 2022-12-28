2b82b84154e34c346b0046f3baa30a714e794484
Ansh <anshagrawal12348@gmail.com>
2022-12-28 22:49:45 +0530

    Updating Files

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var alert = require('alert');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/food');
+  await mongoose.connect('mongodb://127.0.0.1:27017/food');
   
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
